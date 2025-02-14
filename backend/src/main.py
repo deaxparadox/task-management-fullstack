7b5f011745f532df153eb48ba0110be5eb8adc78
deaxparadox <nitish136900kushwaha@gmail.com>
2025-02-14 18:44:07 +0530

    explicit header and method

--- backend/src/main.py
+++ backend/src/main.py
@@ -11,24 +11,34 @@ from .log import logging
 from .routes import index, auth
 
 
-origins = [
+allowed_origins = [
     "http://localhost:3000"
 ]
+allowed_methods = [
+    "GET",
+    "POST",
+    "PUT",
+    "DELETE"
+]
+allowed_headers = {
+    "text/plain",
+    "application/json"
+}
 
 @asynccontextmanager
 async def lifespan(_: FastAPI) -> AsyncIterator[None]:
     redis = aioredis.from_url("redis://localhost")
     FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
-    yield
 
+    yield
 
 app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_methods=["*"],
-    allow_headers=["*"]
+    allow_origins=allowed_origins,
+    allow_methods=allowed_methods,
+    allow_headers=allowed_headers
 )
 
 app.include_router(index.router, prefix="/api")
